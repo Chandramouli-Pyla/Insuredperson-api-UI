ceded12612cf4606fa3a13c7014e7334909b31c8
Chandramouli Pyla <chandramoulipyla459@gmail.com>
2025-09-29 10:33:44 -0400

    Updated Endpoint/fetch URL in ProfilePage

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -24,7 +24,7 @@ export default function ProfilePage() {
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (policyNumber && token) {
-      fetch(`http://localhost:8081/api/insuredpersons/${policyNumber}`, {
+      fetch(`https://insuredperson-api-458668609912.us-central1.run.app/api/insuredpersons/${policyNumber}`, {
         method: "GET",
         headers: {
           "Authorization": `Bearer ${token}`,
